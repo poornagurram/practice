a5eac16a813511df107875067a14d6df0f05615c
punmister <76848636+punmister@users.noreply.github.com>
2021-03-29 21:35:31 +0530

    Create log_normal_distribution.py

---
+++ algorithms/simulation/log_normal_distribution.py
@@ -0,0 +1,30 @@
+from scipy.stats import lognorm
+from scipy import stats as st
+from math import log, sqrt, e
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('WebAgg')
+
+
+def get_dist_params(mu_x, sig_x):
+    mu_dist = log(mu_x**2/sqrt(mu_x**2+sig_x**2))
+    sig_dist = sqrt(log(1+sig_x**2/mu_x**2))
+    return mu_dist, sig_dist
+
+def expiry(x):
+    from datetime import datetime
+    current = datetime.now()
+    delta = (current-x).total_seconds()
+    if int(delta/(60*60*24)) > 180:
+        return False
+    else:
+        return True
+
+if __name__ == '__main__':
+    mu_dist, sig_dist = get_dist_params(45, 10)
+    vals = lognorm.rvs(sig_dist, scale=e**mu_dist, size=100)
+    print(f"95% confidence interval: {st.t.interval(alpha=0.95, df=len(vals) - 1, loc=vals.mean(), scale=st.sem(vals))}")
+    print(f"mean {vals.mean()}, standard deviation : {vals.std()}")
+    plt.hist(vals, 15)
+    plt.show()
+    expiry("")
